f51ba2f445f579087df33da5872231a9ce28ec92
Netiiqs <83643991+Netiiqs@users.noreply.github.com>
2022-01-08 15:15:12 +0100

    different roles, new message

--- commands/reaction.js
+++ commands/reaction.js
@@ -6,27 +6,23 @@ module.exports = {
     permissions: [],
     channelID: '896332399120642118',
     roles: [
-        ["896352132108779531", '899022076122382346'],
-        ["896352224626769971", '899022011580440596'],
-        ["896040564414365726", '899011065038376981'],
+        ["929124211799261244", '929129757960986633'],
+        ["929130745040097291", '929129762138497044'],
     ],
     async execute(message, args, client, discord) {
         let channel = await message.guild.channels.cache.get(this.channelID);
 
         let embed = new MessageEmbed()
             .setColor("#FFFFFF")
-            .setTitle(`Réagissez aux émojis ci-dessous pour avoir vos rôles !`)
-            .setDescription(`__***Explication des rôles :***__
+            .setTitle(`Accès aux différentes sections du Discord DreamFire`)
+            .setDescription(`
 
-            ✘ **Gardien / Prisonnier** → Choisissez l'un des 2 rôles afin d'avoir 
-            accès à l'entièreté du Discord, ils ont les mêmes permissions !
+            <:CSGO:929129757960986633> <@&929124211799261244> → Permet d'accéder à la partie du Discord consacrée
+                                                  à nos serveurs CS:GO !
 
-            ✘ **Notif Ba-Jail** → Quand vous voulez jouer au ba-jail, vous pouvez 
-            ping le rôle dans le channel adéquat afin de ramener des joueurs.
-
-            <:Gardien:899022076122382346> → <@&896352132108779531>
-            <:Prisonnier:899022011580440596> → <@&896352224626769971>
-            <:Notification:899011065038376981> → <@&896040564414365726>
+            <:CSS:929129762138497044> <@&929130745040097291> → Permet d'avoir accès à la section du Discord réservée
+                                             à nos serveurs CS:S !
+            
             `);
 
         if (message.member.hasPermission("ADMINISTRATOR")) {
